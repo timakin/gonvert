0026e074e6636e16763ee7cf9e588da37634d215
timakin <timaki.st@gmail.com>
2017-01-02 01:44:41 +0900

    [fix] converter switching and test

--- gonvert.go
+++ gonvert.go
@@ -4,7 +4,6 @@ import (
 	"github.com/k0kubun/pp"
 	"github.com/saintfish/chardet"
 	c "github.com/timakin/gonvert/converter"
-	"reflect"
 )
 
 type CharCode int
@@ -25,34 +24,12 @@ type CodePair struct {
 	From, To CharCode
 }
 
-var converterCache = make(map[string]reflect.Type)
-
-var codeConverters = make(map[CodePair]string)
-
 func New(text string, toCode CharCode) c.Converter {
 	converter := createConverter(text, toCode)
 	return converter
 }
 
-func prepare() {
-	codeConverters[CodePair{SJIS, UTF8}] = "github.com/timakin/gonvert/converter.SJISToUTF8Converter"
-	codeConverters[CodePair{UTF8, SJIS}] = "github.com/timakin/gonvert/converter.UTF8ToSJISConverter"
-	codeConverters[CodePair{EUCJP, UTF8}] = "github.com/timakin/gonvert/converter.EUCJPToUTF8Converter"
-	codeConverters[CodePair{UTF8, EUCJP}] = "github.com/timakin/gonvert/converter.UTF8ToEUCJPConverter"
-
-	registerConverterCache(c.SJISToUTF8Converter{})
-	registerConverterCache(c.UTF8ToSJISConverter{})
-	registerConverterCache(c.EUCJPToUTF8Converter{})
-	registerConverterCache(c.UTF8ToEUCJPConverter{})
-}
-
-func registerConverterCache(c interface{}) {
-	t := reflect.TypeOf(c)
-	converterCache[t.PkgPath()+"."+t.Name()] = t
-}
-
 func createConverter(text string, toCode CharCode) c.Converter {
-	prepare()
 	charDetector := chardet.NewTextDetector()
 	detectResult, err := charDetector.DetectBest([]byte(text))
 	if err != nil {
@@ -61,18 +38,29 @@ func createConverter(text string, toCode CharCode) c.Converter {
 	fromCode := charcodes[detectResult.Charset]
 	var converter c.Converter
 
-	if fromCode == toCode {
-		converter = &c.DefaultConverter{Text: text}
-	} else {
-		converterStr := codeConverters[CodePair{fromCode, toCode}]
-		t, ok := converterCache[converterStr]
-		if !ok {
-			panic("Reflect error")
+	codepair := CodePair{fromCode, toCode}
+
+	switch codepair {
+	case (CodePair{UTF8, SJIS}):
+		{
+			converter = &c.UTF8ToSJISConverter{Text: text}
 		}
-		pp.Print(t)
-		v := reflect.New(t).Interface()
-		pp.Print(v)
-		converter = &c.DefaultConverter{Text: text}
+	case (CodePair{SJIS, UTF8}):
+		{
+			converter = &c.SJISToUTF8Converter{Text: text}
+		}
+	case (CodePair{UTF8, EUCJP}):
+		{
+			converter = &c.UTF8ToEUCJPConverter{Text: text}
+		}
+	case (CodePair{EUCJP, UTF8}):
+		{
+			converter = &c.EUCJPToUTF8Converter{Text: text}
+		}
+	default:
+		{
+			converter = &c.DefaultConverter{Text: text}
+		}
 	}
 
 	return converter
