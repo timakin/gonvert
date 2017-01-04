ce610b33d7a749e9d3cd38773c622e1a12c280b4
timakin <timaki.st@gmail.com>
2017-01-04 20:28:39 +0900

    [fix] delete useless comments

--- converter/converter.go
+++ converter/converter.go
@@ -7,17 +7,3 @@ type Converter interface {
 type ConversionPattern struct {
 	TextByte []byte
 }
-
-// func transformEncoding(rawReader io.Reader, trans transform.Transformer) (string, error) {
-// 	ret, err := ioutil.ReadAll(transform.NewReader(rawReader, trans))
-// 	if err == nil {
-// 		return string(ret), nil
-// 	} else {
-// 		return "", err
-// 	}
-// }
-//
-// // Convert a string encoding from ShiftJIS to UTF-8
-// func FromShiftJIS(str string) (string, error) {
-// 	return transformEncoding(strings.NewReader(str), japanese.ShiftJIS.NewDecoder())
-// }
