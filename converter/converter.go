063ff632b6e682a824dc2e49b8a75ac9370a7735
timakin <timaki.st@gmail.com>
2017-01-06 09:40:42 +0900

    [add] meditator

--- converter/converter.go
+++ converter/converter.go
@@ -22,3 +22,17 @@ func transformEncoding(rawReader *bytes.Reader, trans transform.Transformer) (st
 		return "", err
 	}
 }
+
+func transformEncodingWithMeditation(rawReader *bytes.Reader, decoder transform.Transformer, encoder transfrom.Transformer) (string, error) {
+	decoded, err := ioutil.ReadAll(transform.NewReader(rawReader, decoder))
+	if err == nil {
+		encoded, err := ioutil.ReadAll(transform.NewReader(rawReader, encoder))
+		if err == nil {
+			return string(encoded), nil
+		} else {
+			return "", err
+		}
+	} else {
+		return "", err
+	}
+}
