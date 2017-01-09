d6c543440503a1f3258daca32c196755e94ffaa7
timakin <timaki.st@gmail.com>
2017-01-10 08:15:52 +0900

    [fix] decode utf16 to utf8

--- converter/utf16_to_utf8.go
+++ converter/utf16_to_utf8.go
@@ -2,11 +2,26 @@ package converter
 
 import (
 	"bytes"
-	"golang.org/x/text/encoding/japanese"
+	"unicode/utf16"
+	"unicode/utf8"
 )
 
 type UTF16ToUTF8Converter ConversionPattern
 
 func (c *UTF16ToUTF8Converter) Convert() (string, error) {
-	return transformEncoding(bytes.NewReader(c.TextByte), japanese.ShiftJIS.NewEncoder())
+	u16s := make([]uint16, 1)
+
+	ret := &bytes.Buffer{}
+
+	b8buf := make([]byte, 4)
+
+	lb := len(c.TextByte)
+	for i := 0; i < lb; i += 2 {
+		u16s[0] = uint16(b[i]) + (uint16(b[i+1]) << 8)
+		r := utf16.Decode(u16s)
+		n := utf8.EncodeRune(b8buf, r[0])
+		ret.Write(b8buf[:n])
+	}
+
+	return ret.String(), nil
 }
